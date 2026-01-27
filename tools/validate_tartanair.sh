df38fcf78eabda0263b57a3f656f30eac8688045
Andy Roberts <andymrobertsuk@gmail.com>
2026-01-27 20:34:31 +0000

    added power measurement and test of single scene in tartanair dataset

--- tools/validate_tartanair.sh
+++ tools/validate_tartanair.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-TARTANAIR_PATH=datasets/TartanAir
+TARTANAIR_PATH=/home/campus.ncl.ac.uk/c4071391/datasets/agricultural/tartanair
 
 python evaluation_scripts/validate_tartanair.py --datapath=$TARTANAIR_PATH --weights=droid.pth --disable_vis $@
 
